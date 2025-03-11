f12ffb5a296a160c15573b5a94e806f2252dd97d
DevRaval2604 <devraval2004@gmail.com>
2025-03-12 00:13:09 +0530

    Welcome to AcaAssist

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,7 +46,7 @@ flutter {
 dependencies {
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
-    // TODO: Add the dependencies for Firebase products you want to use
+    // Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
     // Add the dependencies for any other desired Firebase products
